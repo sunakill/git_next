56df598f72d6e5cab18545af62b5979052a24204
remy <pennywise@MacBook-Pro-de-remy.local>
2017-12-09 01:31:39 +0100

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,10 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE  1
+
+# include "./libft/libft.h"
+# include <unistd.h>
+
+int			get_next_line(int const fd, char **line);
+
+#endif
